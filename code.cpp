9fa704e1c3a65301b7fa4fc4f7282ed0fea7d326
Abdul Wasay <110778511+abdulwasay100@users.noreply.github.com>
2024-06-30 13:26:18 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 
-
 using namespace std;
+
 
 int main() 
 {
-    int n = 10, t1 = 0, t2 = 1, the_next_Term;
+    int no = 10, t1 = 0, t2 = 1, the_next_Term;
 
-    for (int i = 1; i <= n; ++i) {
+    
+    for (int i = 1; i <= no; ++i) 
+        
+    {
         cout << t1 << " ";
+        
         the_next_Term = t1 + t2;
+        
         t1 = t2;
+        
         t2 = the_next_Term;
     }
 
+    
     return 0;
 }
