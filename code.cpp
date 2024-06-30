20e85cffe330f5e1f5702d066f3681271ffec529
Abdul Wasay <110778511+abdulwasay100@users.noreply.github.com>
2024-06-30 12:50:20 +0500

    Update code.cpp
    next term to thenextterm

--- code.cpp
+++ code.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-    int n = 10, t1 = 0, t2 = 1, nextTerm;
+    int n = 10, t1 = 0, t2 = 1, thenextTerm;
 
     for (int i = 1; i <= n; ++i) {
         cout << t1 << " ";
-        nextTerm = t1 + t2;
+        thenextTerm = t1 + t2;
         t1 = t2;
-        t2 = nextTerm;
+        t2 = thenextTerm;
     }
 
     return 0;
